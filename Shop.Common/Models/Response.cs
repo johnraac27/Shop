acaf5d22ef5d8c5850ed2eb439c41b9cace501bf
johnraac27 <36141932+johnraac27@users.noreply.github.com>
2019-02-24 23:29:55 -0500

    API Service Continue

--- Shop.Common/Models/Response.cs
+++ Shop.Common/Models/Response.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Shop.Common.Models
+﻿namespace Shop.Common.Models
 {
-    class Response
+    public class Response
     {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
     }
 }
